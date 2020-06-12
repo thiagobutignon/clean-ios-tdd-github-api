32a7423e95c16b393b441b86cfb26b2221635a95
Thiago Butignon <thiagobutignon@gmail.com>
2020-06-12 20:38:55 -0300

    Create sut for AlamofireAdaptersTests

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,28 @@
+//
+//  AlamofireAdapterTests.swift
+//  InfraTests
+//
+//  Created by Thiago B Claramunt on 12/06/20.
+//  Copyright © 2020 Thiago B Claramunt. All rights reserved.
+//
+
+import XCTest
+import Data
+import Infra
+import Alamofire
+
+class AlamofireAdapterTests: XCTestCase {
+
+
+}
+
+extension AlamofireAdapterTests {
+    func makeSut(file: StaticString = #file, line: UInt = #line) -> AlamofireAdapter {
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [UrlProtocolStub.self]
+        let session = Session(configuration: configuration)
+        let sut = AlamofireAdapter(session: session)
+        checkMemoryLeak(for: sut, file: file, line: line)
+        return sut
+    }
+}
