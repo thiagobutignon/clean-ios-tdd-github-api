89168ae094044a1e66b6de215289ff23a7ccc89f
Thiago Butignon <thiagobutignon@gmail.com>
2020-06-12 19:15:09 -0300

    Ensure Get method calls correct url

---
+++ DataTests/UseCases/RemoteGetIssuesTests.swift
@@ -0,0 +1,48 @@
+//
+//  RemoteGetIssuesTests.swift
+//  DataTests
+//
+//  Created by Thiago B Claramunt on 12/06/20.
+//  Copyright © 2020 Thiago B Claramunt. All rights reserved.
+//
+
+import XCTest
+import Data
+import Domain
+
+class RemoteGetIssuesTests: XCTestCase {
+    func test_get_should_call_httpClient_with_correct_url() {
+        let url = makeUrl()
+        let (sut, httpClientSpy) = makeSut(url: url)
+        sut.get { _ in }
+        XCTAssertEqual(httpClientSpy.urls, [url])
+    }
+    
+}
+
+extension RemoteGetIssuesTests {
+    func makeSut(url: URL = URL(string: "http://any-url.com")!, file: StaticString = #file, line: UInt = #line) -> (sut: RemoteGetIssues, httpClientSpy: HttpClientSpy) {
+        let httpClientSpy = HttpClientSpy()
+        let sut = RemoteGetIssues(url: url, httpClient: httpClientSpy)
+        checkMemoryLeak(for: sut, file: file, line: line)
+        checkMemoryLeak(for: httpClientSpy, file: file, line: line)
+        return (sut, httpClientSpy)
+    }
+    
+    func expect(_ sut: RemoteGetIssues, completeWith expectedResult: GetIssues.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "waiting")
+        sut.get { receivedResult in
+            switch (expectedResult, receivedResult) {
+                case (.failure(let expectedError), .failure(let receivedError)):
+                    XCTAssertEqual(expectedError, receivedError, file: file, line: line)
+                case (.success(let expectedIssues), .success(let receivedIssues)):
+                    XCTAssertEqual(expectedIssues, receivedIssues, file: file, line: line)
+                default:
+                    XCTFail("Expected \(expectedResult) error received \(receivedResult) instead", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        action()
+        wait(for: [exp], timeout: 1)
+    }
+}
