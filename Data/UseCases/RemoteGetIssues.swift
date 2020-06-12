649981a3b7f757dc847bfadc9c0645740244beb7
Thiago Butignon <thiagobutignon@gmail.com>
2020-06-12 15:10:02 -0300

    Injection Domain Layer into Data

---
+++ Data/UseCases/RemoteGetIssues.swift
@@ -0,0 +1,10 @@
+//
+//  RemoteGetIssues.swift
+//  Data
+//
+//  Created by Thiago B Claramunt on 12/06/20.
+//  Copyright © 2020 Thiago B Claramunt. All rights reserved.
+//
+
+import Foundation
+import Domain
