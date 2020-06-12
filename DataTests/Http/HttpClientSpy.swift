84bbd392a208f6da90eefafb8282ca9cfd337592
Thiago Butignon <thiagobutignon@gmail.com>
2020-06-12 15:22:00 -0300

    Create HttpClientSpy on Data

---
+++ DataTests/Http/HttpClientSpy.swift
@@ -0,0 +1,28 @@
+//
+//  HttpClientSpy.swift
+//  DataTests
+//
+//  Created by Thiago B Claramunt on 12/06/20.
+//  Copyright © 2020 Thiago B Claramunt. All rights reserved.
+//
+
+import Foundation
+import Data
+
+class HttpClientSpy: HttpGetClient {
+    var urls: [URL] = [URL]()
+    var completion: ((Result<Data?, HttpError>) -> Void)?
+    
+    func get(to url: URL, completion: @escaping (Result<Data?, HttpError>) -> Void) {
+        self.urls.append(url)
+        self.completion = completion
+    }
+    
+    func completeWithError(_ error: HttpError) {
+        completion?(.failure(error))
+    }
+    
+    func completeWithSuccess(_ data: Data) {
+        completion?(.success(data))
+    }
+}
