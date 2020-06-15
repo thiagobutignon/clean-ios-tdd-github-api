9ecb635b4192a608f16ac96d5e444ff552687ac0
Thiago Butignon <thiagobutignon@gmail.com>
2020-06-14 22:20:21 -0300

    create GetIssueDetail

---
+++ Data/UseCases/RemoteGetIssueDetail.swift
@@ -0,0 +1,40 @@
+//
+//  RemoteGetIssueDetail.swift
+//  Data
+//
+//  Created by Thiago B Claramunt on 14/06/20.
+//  Copyright © 2020 Thiago B Claramunt. All rights reserved.
+//
+
+import Foundation
+import Domain
+
+public final class RemoteGetIssueDetail: GetIssueDetail {
+    
+    private let url: URL
+    private let httpClient: HttpGetClient
+    
+    public init(url: URL, httpClient: HttpGetClient) {
+        self.url = url
+        self.httpClient = httpClient
+    }
+    
+    public func get(completion: @escaping (GetIssueDetail.Result) -> Void) {
+        httpClient.get(to: url) { [weak self] result in
+            guard self != nil else { return }
+            switch result {
+                case .success(let data):
+                    if let model: Issue = data?.toModel() {
+                        print(model)
+                        completion(.success(model))
+                    } else {
+                        completion(.failure(.unexpected))
+                }
+                
+                case .failure:
+                    print("---- deu erro 458x")
+                    completion(.failure(.unexpected))
+            }
+        }
+    }
+}
