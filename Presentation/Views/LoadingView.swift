3badaf982fb8369e010d8c4d264409f15cbbfa0a
Thiago Butignon <thiagobutignon@gmail.com>
2020-06-14 22:49:38 -0300

    create AlertView and LoadingView

---
+++ Presentation/Views/LoadingView.swift
@@ -0,0 +1,21 @@
+//
+//  LoadingView.swift
+//  Presentation
+//
+//  Created by Thiago B Claramunt on 14/06/20.
+//  Copyright © 2020 Thiago B Claramunt. All rights reserved.
+//
+
+import Foundation
+
+public protocol LoadingView {
+    func display(viewModel: LoadingViewModel)
+}
+
+public struct LoadingViewModel: Equatable {
+    public var isLoading: Bool
+    
+    public init(isLoading: Bool) {
+        self.isLoading = isLoading
+    }
+}
