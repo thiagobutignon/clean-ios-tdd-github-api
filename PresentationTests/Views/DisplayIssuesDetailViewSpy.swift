1c6de16308836dc9811ec07cf832f47395a2c892
Thiago Butignon <thiagobutignon@gmail.com>
2020-06-15 01:02:34 -0300

    create spy for DisplayIssuesDetailView

---
+++ PresentationTests/Views/DisplayIssuesDetailViewSpy.swift
@@ -0,0 +1,22 @@
+//
+//  DisplayIssuesDetailViewSpy.swift
+//  PresentationTests
+//
+//  Created by Thiago B Claramunt on 15/06/20.
+//  Copyright © 2020 Thiago B Claramunt. All rights reserved.
+//
+
+import Foundation
+import Presentation
+
+class DisplayIssuesDetailViewSpy: DisplayIssueDetailView {
+    var emit: ((DisplayIssueDetailViewModel) -> Void)?
+    
+    func observe(completion: @escaping (DisplayIssueDetailViewModel) -> Void) {
+        self.emit = completion
+    }
+    
+    func showIssueDetail(viewModel: DisplayIssueDetailViewModel) {
+        self.emit?(viewModel)
+    }
+}
