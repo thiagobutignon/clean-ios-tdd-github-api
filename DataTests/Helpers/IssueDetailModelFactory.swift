77b27a8fe90205c8e707512c29f963842dad0837
Thiago Butignon <thiagobutignon@gmail.com>
2020-06-14 22:23:09 -0300

    create factory for IssueDetailModel

---
+++ DataTests/Helpers/IssueDetailModelFactory.swift
@@ -0,0 +1,18 @@
+//
+//  IssueDetailModelFactory.swift
+//  DataTests
+//
+//  Created by Thiago B Claramunt on 14/06/20.
+//  Copyright © 2020 Thiago B Claramunt. All rights reserved.
+//
+
+import Foundation
+import Domain
+
+func makeIssueDetailModelFactory() -> IssueDetail {
+    return IssueDetail(id: 0, number: 0, url: "any_url", title: "any_title", body: "any_body", createdAt: "any_createdAt", user: makeUser())
+}
+
+func makeUser() -> User {
+    return User(id: 0, avatarUrl: "any_avatarUrl")
+}
